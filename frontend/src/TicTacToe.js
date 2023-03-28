2be0dbfece3cf7bf4c3d4ab2f50c6bf554a467df
miho nakayama <miho.nakayam@gmail.com>
2023-03-28 11:19:15 +0900

    add TicTacToe game

---
+++ frontend/src/TicTacToe.js
@@ -0,0 +1,126 @@
+import React from "react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <Button
+      variant="outlined"
+      component="label"
+      size="large"
+      sx={{ width: "90px", height: "90px", fontSize: "x-large" }}
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </Button>
+  );
+}
+
+function Board({ oysterIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (oysterIsNext) {
+      nextSquares[i] = "🥂";
+    } else {
+      nextSquares[i] = "🦪";
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (oysterIsNext ? "🦪" : "🥂");
+  }
+
+  return (
+    <>
+      <p>{status}</p>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default function TicTacToe() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const oysterIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function JumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <Button onClick={() => JumpTo(move)}>{description}</Button>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <Board
+        oysterIsNext={oysterIsNext}
+        squares={currentSquares}
+        onPlay={handlePlay}
+      />
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
